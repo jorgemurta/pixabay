6bb027a0f71bc74133317ec04d5a05e51fc0208a
Jorge Murta <jmurta.jm@gmail.com>
2017-03-21 17:14:06 +0100

    New Categories

--- src/Constants/Categories.php
+++ src/Constants/Categories.php
@@ -7,6 +7,23 @@ class Categories
     const FASHION = 'fashion';
     const NATURE = 'nature';
     const BACKGROUNDS = 'backgrounds';
+    const SCIENCE = 'science';
+    const EDUCATION = 'education';
+    const PEOPLE = 'people';
+    const FEELINGS = 'feelings';
+    const RELIGION = 'religion';
+    const HEALTH = 'health';
+    const PLACES = 'places';
+    const ANIMALS = 'animals';
+    const INDUSTRY = 'industry';
+    const FOOD = 'food';
+    const COMPUTER = 'computer';
+    const SPORTS = 'sports';
+    const TRANSPORTATION = 'transportation';
+    const TRAVEL = 'travel';
+    const BUILDINGS = 'buildings';
+    const BUSINESS = 'business';
+    const MUSIC = 'music';
 
     /**
      * Return all the constants in this class
